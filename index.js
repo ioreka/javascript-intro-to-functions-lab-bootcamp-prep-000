151bc532b99f2f1842896405b2800f9b5121041d
Lucy Mitchell <lucymhodges@gmail.com>
2018-04-20 21:17:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,28 +16,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    console.log("I can't hear you!")
-} else  {
-    console.log("Expected the name to be Susan")
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma") {
+    return "I love you, too."
+  }
 }
-  return string
-}
-
-
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
 
 
 
